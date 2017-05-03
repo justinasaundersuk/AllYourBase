d53e78bda942b60856e605511af03477f817c483
Justin Saunders <justin.saunders@dam-digital.com>
2017-05-03 09:44:27 +0100

    Fixed filter styles, fixed flip card back style

--- AllYourBase/Scripts/custom.js
+++ AllYourBase/Scripts/custom.js
@@ -5,3 +5,12 @@ $('.filter-button').on('click', function () {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
 });
+
+$(function () {
+    $('.flip-container .back').matchHeight({
+        byRow: true,
+        property: 'height',
+        target: $('.flip-container .front'),
+        remove: false
+    });
+});
