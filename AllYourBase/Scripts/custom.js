87365e0e3da81a2b2b4eab1959a1f83b764ce081
Justin Saunders <justin.saunders@dam-digital.com>
2017-05-03 11:30:52 +0100

    Tons of responsive fixes and tidyups

--- AllYourBase/Scripts/custom.js
+++ AllYourBase/Scripts/custom.js
@@ -1,16 +1,38 @@
 ﻿
-// filter items on button click
-$('.filter-button').on('click', function () {
-    var $grid = $('.grid');
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-});
+$(window).load(function () {
+    // initial isotope load
+    $('.grid').isotope({
+        percentPosition: true,
+        itemSelector: '.isotope-item',
+        layoutMode: 'fitRows'
+    });
 
-$(function () {
+    // set height of flip back panel to same height as front (the image)
     $('.flip-container .back').matchHeight({
-        byRow: true,
+        byRow: false,
         property: 'height',
-        target: $('.flip-container .front'),
+        target: $('.card-image'),
         remove: false
     });
 });
+
+// filter items on button click
+$('.filter-button').on('click', function (e) {
+    e.preventDefault();
+    var $grid = $('.grid');
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({
+        filter: filterValue,
+        transitionDuration: '1s',
+        stagger: 30
+    });
+    // set button active state
+    $('.filter-button').removeClass('active');
+    $(this).addClass('active');
+});
+
+// handle the flip toggle
+$(".flip-container").on('mouseenter mouseleave click', function () {
+    $(this).toggleClass('hover');
+});
+
